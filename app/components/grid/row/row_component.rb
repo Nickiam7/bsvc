82abc1c48048818754fa45c87f46380e00cc454c
nmcneany <nmcneany@gmail.com>
2023-01-08 14:33:43 -0600

    remove duplicate directory

--- app/components/grid/row/row_component.rb
+++ app/components/grid/row/row_component.rb
@@ -3,7 +3,7 @@
 module Grid
   module Row
     class RowComponent < ViewComponent::Base
-      renders_many :columns, 'ColumnsComponent'
+      renders_many :columns, Columns::ColumnsComponent
 
       def initialize(**options)
         @classes = options[:classes]
@@ -22,47 +22,6 @@ module Grid
 
       def default_plus_passed_classes
         "row #{@classes}".strip
-      end
-
-      class ColumnsComponent < ViewComponent::Base
-        BREAK_POINTS = [:xs, :sm, :md, :lg, :xl, :xxl]
-
-        def initialize(**options)
-          @columns = options[:col]
-          @offsets = options[:offset]
-          @classes = options[:classes]
-          @styles = options[:styles]
-        end
-
-        def call
-          content_tag(:div,
-            content,
-            class: column_class_builder,
-            style: @styles,
-          )
-        end
-
-        private
-
-        def column_class_builder
-          "#{column_breakpoint} #{column_offset} #{@classes}"
-        end
-
-        def class_parser(option, klass)
-          option.map do |key, value|
-            if BREAK_POINTS.include?(key)
-              "#{klass}-#{key}-#{value} "
-            end
-          end.join('').strip
-        end
-
-        def column_breakpoint
-          class_parser(@columns, 'col')
-        end
-
-        def column_offset
-          class_parser(@offsets, 'offset')
-        end
       end
     end
   end
